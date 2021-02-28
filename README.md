785b4a6c0813c2c37a7bdd6803cfd5b1ca9ba24e
Yoshimura Yuu <yyu@mental.poker>
2021-02-28 23:01:01 +0900

    Add CI

---
+++ README.md
@@ -0,0 +1,10 @@
+Zenn.dev
+==============================================
+
+[![CI](https://github.com/y-yu/zenn.dev/actions/workflows/ci.yml/badge.svg)](https://github.com/y-yu/zenn.dev/actions/workflows/ci.yml)
+
+Source code of [zenn.dev/yyu](https://zenn.dev/yyu).
+
+## Preview
+
+Access to `https://y-yu.github.io/zenn.dev/articles/{article file name}.html`.
---
+++ docker/wget.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+set -x
+
+npx zenn preview &
+sleep 2
+
+if [ -d "./targets" ]; then
+  rm -rf ./targets
+fi
+
+mkdir targets
+
+wget -P ./targets http://localhost:8000/
+
+for article in ./articles/*.md; do
+  if [[ $article =~ articles/(.*)\.md ]]; then
+    wget -p -H -E -nH -k -P ./targets "http://localhost:8000/articles/${BASH_REMATCH[1]}"
+  fi
+done
+
+exit 0
