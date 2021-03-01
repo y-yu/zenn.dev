6d14546601574d9e9f867ba651b89f903c6f49d0
Yoshimura Yuu <yyu@mental.poker>
2021-03-01 12:08:41 +0900

    Fix wget.sh

--- .github/workflows/wget.sh
+++ .github/workflows/wget.sh
@@ -4,17 +4,32 @@ set -x
 
 sleep 2
 
-if [ -d "./targets" ]; then
-  rm -rf ./targets
+if [ ! -d "./targets" ]; then
+  mkdir targets
 fi
 
-mkdir targets
-
-wget -P ./targets http://localhost:8000/
+wget --mirror \
+  --quiet --show-progress \
+  --no-parent \
+  --convert-links \
+  --no-host-directories \
+  --adjust-extension \
+  --execute robots=off \
+  --directory-prefix=./targets \
+  http://localhost:8000/
 
 for article in ./articles/*.md; do
   if [[ $article =~ articles/(.*)\.md ]]; then
-    wget -p -H -E -nH -k -P ./targets "http://localhost:8000/articles/${BASH_REMATCH[1]}"
+    wget --mirror \
+      --page-requisites \
+      --quiet --show-progress \
+      --no-parent \
+      --convert-links \
+      --no-host-directories \
+      --adjust-extension \
+      --execute robots=off \
+      --directory-prefix=./targets \
+      "http://localhost:8000/articles/${BASH_REMATCH[1]}"
   fi
 done
 
