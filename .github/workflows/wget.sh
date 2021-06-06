b9f53f1e3405f3f4325ae1046015cf4f21d6f260
Yoshimura Yuu <yyu@mental.poker>
2021-06-07 02:11:35 +0900

    Update CI setting for KaTeX

--- .github/workflows/wget.sh
+++ .github/workflows/wget.sh
@@ -5,8 +5,10 @@ set -x
 sleep 2
 
 if [ ! -d "./targets" ]; then
-  mkdir targets
+  mkdir -p targets/_next
 fi
+
+cp -r ./node_modules/zenn-cli/.next/static ./targets/_next/
 
 wget --mirror \
   --quiet --show-progress \
@@ -32,5 +34,14 @@ for article in ./articles/*.md; do
       "http://localhost:8000/articles/${BASH_REMATCH[1]}"
   fi
 done
+
+cd ./targets
+ 
+find . -name "*.html" -or -name "*.css" -or -name "*.js" |\
+    xargs perl -i -pe 's/(?<!\.)\/(_next)/..\/$1/g'
+
+# Update KaTeX version
+find . -name "*.css" -or -name "*.js" |\
+    xargs perl -i -pe 's/0\.12\.0/0.13.11/g'
 
 exit 0
